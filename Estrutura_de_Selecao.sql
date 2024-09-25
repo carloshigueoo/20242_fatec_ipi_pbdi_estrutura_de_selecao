ae0861883829d0c97ff12b89d135d025e2727d12
Carlos Kinjo <carloshigueoo@gmail.com>
2024-09-25 17:42:10 -0300

    feat: solves the exercise 1.3

--- Estrutura_de_Selecao.sql
+++ Estrutura_de_Selecao.sql
@@ -16,68 +16,68 @@
 -- 1.1 Faça um programa que exibe se um número inteiro é múltiplo de 3.
 
 -- SOLUÇÃO IF
--- DO
--- $$ 
--- DECLARE
---     numero int := valor_aleatorio_entre(1,300);
--- BEGIN 
---     IF numero % 3 = 0 THEN 
---         RAISE NOTICE 'O número % é mútiplo de 3', numero;
---     ELSE 
---         RAISE NOTICE 'O número % não é multiplo de 3', numero;
---     END IF;
--- END
--- $$; 
+DO
+$$ 
+DECLARE
+    numero int := valor_aleatorio_entre(1,300);
+BEGIN 
+    IF numero % 3 = 0 THEN 
+        RAISE NOTICE 'O número % é mútiplo de 3', numero;
+    ELSE 
+        RAISE NOTICE 'O número % não é multiplo de 3', numero;
+    END IF;
+END
+$$; 
 
 -- SOLUÇÃO CASE
--- DO
--- $$ 
--- DECLARE
---     numero int := valor_aleatorio_entre(1,300);
--- BEGIN 
---     CASE
---         WHEN numero % 3 = 0 THEN 
---             RAISE NOTICE 'O número % é mútiplo de 3', numero;
---         ELSE 
---             RAISE NOTICE 'O número % não é multiplo de 3', numero;
---     END CASE;
--- END
--- $$;
+DO
+$$ 
+DECLARE
+    numero int := valor_aleatorio_entre(1,300);
+BEGIN 
+    CASE
+        WHEN numero % 3 = 0 THEN 
+            RAISE NOTICE 'O número % é mútiplo de 3', numero;
+        ELSE 
+            RAISE NOTICE 'O número % não é multiplo de 3', numero;
+    END CASE;
+END
+$$;
 
 -- 1.2 Faça um programa que exibe se um número inteiro é múltiplo de 3 ou de 5.
 
 -- SOLUÇÃO IF
--- DO
--- $$ 
--- DECLARE
---     numero int := valor_aleatorio_entre(1,100);
--- BEGIN 
---     IF numero % 3 = 0 THEN 
---         RAISE NOTICE 'O número % é mútiplo de 3', numero;
---     ELSEIF numero % 5 = 0 THEN 
---         RAISE NOTICE 'O número % é mútiplo de 5', numero;
---     ELSE 
---         RAISE NOTICE 'O número % não é multiplo de 3 e nem de 5', numero;
---     END IF;
--- END
--- $$; 
+DO
+$$ 
+DECLARE
+    numero int := valor_aleatorio_entre(1,100);
+BEGIN 
+    IF numero % 3 = 0 THEN 
+        RAISE NOTICE 'O número % é mútiplo de 3', numero;
+    ELSEIF numero % 5 = 0 THEN 
+        RAISE NOTICE 'O número % é mútiplo de 5', numero;
+    ELSE 
+        RAISE NOTICE 'O número % não é multiplo de 3 e nem de 5', numero;
+    END IF;
+END
+$$; 
 
--- SOLUCAO CASE
--- DO
--- $$ 
--- DECLARE
---     numero int := valor_aleatorio_entre(1,100);
--- BEGIN 
---     CASE
---         WHEN numero % 3 = 0 THEN 
---             RAISE NOTICE 'O número % é mútiplo de 3', numero;
---         WHEN numero % 5 = 0 THEN 
---             RAISE NOTICE 'O número % é mútiplo de 5', numero;
---         ELSE 
---             RAISE NOTICE 'O número % não é multiplo de 3 e nem de 5', numero;
---     END CASE;
--- END
--- $$; 
+-- SOLUÇÃO CASE
+DO
+$$ 
+DECLARE
+    numero int := valor_aleatorio_entre(1,100);
+BEGIN 
+    CASE
+        WHEN numero % 3 = 0 THEN 
+            RAISE NOTICE 'O número % é mútiplo de 3', numero;
+        WHEN numero % 5 = 0 THEN 
+            RAISE NOTICE 'O número % é mútiplo de 5', numero;
+        ELSE 
+            RAISE NOTICE 'O número % não é multiplo de 3 e nem de 5', numero;
+    END CASE;
+END
+$$; 
 
 -- 1.3 Faça um programa que opera de acordo com o seguinte menu.
 -- Opções:
@@ -90,10 +90,69 @@
 -- Exemplo:
 -- 2 + 3 = 5
 
+-- SOLUÇÃO IF
+DO
+$$
+DECLARE
+    op1 int := valor_aleatorio_entre (1,100);
+    op2 int := valor_aleatorio_entre (1,100);
+    opcao int := valor_aleatorio_entre (1,5);
+    resultado numeric;
+BEGIN
+    IF opcao = 1 THEN -- SOMA 
+        resultado := op1 + op2;
+        RAISE NOTICE '% + % = %', op1 , op2 , resultado;
+    ELSEIF opcao = 2 THEN -- SUBTRAÇÃO 
+        resultado := op1 - op2;
+        RAISE NOTICE '% - % = %', op1 , op2 , resultado;
+    ELSEIF opcao = 3 THEN -- MULTIPLICAÇÃO 
+        resultado := op1 * op2;
+        RAISE NOTICE '% * % = %', op1 , op2 , resultado;
+    ELSEIF opcao = 4 THEN -- DIVISÃO
+        IF op2 = 0 THEN
+            RAISE NOTICE 'Divisão por 0 inválida';
+        ELSE
+            resultado := op1 / op2;
+            RAISE NOTICE '% / % = %', op1 , op2 , resultado;
+        END IF;
+    ELSE
+        RAISE NOTICE 'Opção inválida';
+    END IF;
+END 
+$$;
 
-
-
-
+-- SOLUÇÃO CASE 
+DO
+$$
+DECLARE
+    op1 int := valor_aleatorio_entre (1,100);
+    op2 int := valor_aleatorio_entre (1,100);
+    opcao int := valor_aleatorio_entre (1,5);
+    resultado numeric;
+BEGIN
+    CASE
+        WHEN opcao = 1 THEN -- SOMA 
+            resultado := op1 + op2;
+            RAISE NOTICE '% + % = %', op1 , op2 , resultado;
+        WHEN opcao = 2 THEN -- SUBTRAÇÃO 
+            resultado := op1 - op2;
+            RAISE NOTICE '% - % = %', op1 , op2 , resultado;
+        WHEN opcao = 3 THEN -- MULTIPLICAÇÃO 
+            resultado := op1 * op2;
+            RAISE NOTICE '% * % = %', op1 , op2 , resultado;
+        WHEN opcao = 4 THEN -- DIVISÃO
+            CASE
+                WHEN op2 = 0 THEN
+                    RAISE NOTICE 'Divisão por 0 inválida';
+                ELSE
+                    resultado := op1 / op2;
+                    RAISE NOTICE '% / % = %', op1 , op2 , resultado;
+            END CASE;
+        ELSE
+            RAISE NOTICE 'Opção inválida';
+    END CASE;
+END 
+$$;
 
 -- 1.4 Um comerciante comprou um produto e quer vendê-lo com um lucro de 45% se o valor
 -- da compra for menor que R$20. Caso contrário, ele deseja lucro de 30%. Faça um
