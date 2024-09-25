f8111f120bfc814c2f900586461af44f2f14afc1
Carlos Kinjo <carloshigueoo@gmail.com>
2024-09-25 16:39:50 -0300

    feat: solves the exercise 1.2

--- Estrutura_de_Selecao.sql
+++ Estrutura_de_Selecao.sql
@@ -46,17 +46,38 @@
 
 -- 1.2 Faça um programa que exibe se um número inteiro é múltiplo de 3 ou de 5.
 
+-- SOLUÇÃO IF
+-- DO
+-- $$ 
+-- DECLARE
+--     numero int := valor_aleatorio_entre(1,100);
+-- BEGIN 
+--     IF numero % 3 = 0 THEN 
+--         RAISE NOTICE 'O número % é mútiplo de 3', numero;
+--     ELSEIF numero % 5 = 0 THEN 
+--         RAISE NOTICE 'O número % é mútiplo de 5', numero;
+--     ELSE 
+--         RAISE NOTICE 'O número % não é multiplo de 3 e nem de 5', numero;
+--     END IF;
+-- END
+-- $$; 
 
-
-
-
-
-
-
-
-
-
-
+-- SOLUCAO CASE
+-- DO
+-- $$ 
+-- DECLARE
+--     numero int := valor_aleatorio_entre(1,100);
+-- BEGIN 
+--     CASE
+--         WHEN numero % 3 = 0 THEN 
+--             RAISE NOTICE 'O número % é mútiplo de 3', numero;
+--         WHEN numero % 5 = 0 THEN 
+--             RAISE NOTICE 'O número % é mútiplo de 5', numero;
+--         ELSE 
+--             RAISE NOTICE 'O número % não é multiplo de 3 e nem de 5', numero;
+--     END CASE;
+-- END
+-- $$; 
 
 -- 1.3 Faça um programa que opera de acordo com o seguinte menu.
 -- Opções:
