f46ba8c0c2d5c1aa599a586fb77328f65a6bd5fa
Carlos Kinjo <carloshigueoo@gmail.com>
2024-09-26 20:41:29 -0300

    feat: solves the exercise 1.5

--- Estrutura_de_Selecao.sql
+++ Estrutura_de_Selecao.sql
@@ -158,7 +158,7 @@ $$;
 -- da compra for menor que R$20. Caso contrário, ele deseja lucro de 30%. Faça um
 -- programa que, dado o valor do produto, calcula o valor de venda.
 
---SOLUÇÃO IF
+-- SOLUÇÃO IF
 DO
 $$
 DECLARE
@@ -185,11 +185,104 @@ BEGIN
     CASE
         WHEN compra <= 20 THEN
             venda := compra * 1.45;
-    ELSE
-        venda := compra * 1.3;
+        ELSE
+            venda := compra * 1.3;
     END CASE;
     RAISE NOTICE 'compra: R$ %', compra;
     RAISE NOTICE 'venda: R$ %', venda;
     END
 $$;
 
+-- 1.5 Resolva o problema disponível no link a seguir.
+-- https://www.beecrowd.com.br/judge/en/p
+
+-- A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
+
+-- Salário            | Percentual de Reajuste
+----------------------------------------------
+-- 0 - 400.00         | 15%
+-- 400.01 - 800.00    | 12%
+-- 800.01 - 1200.00   | 10%
+-- 1200.01 - 2000.00  | 7%
+-- Acima de 2000.00   | 4%
+-- Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+
+-- Entrada
+-- A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
+
+-- Saída
+-- Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste (ambos devem ser apresentados com 2 casas decimais) e o percentual de reajuste ganho, conforme exemplo abaixo.
+
+-- Exemplo de Entrada	Exemplo de Saída
+-- 400.00
+
+-- Novo salario: 460.00
+-- Reajuste ganho: 60.00
+-- Em percentual: 15 %
+
+-- 800.01
+
+-- Novo salario: 880.01
+-- Reajuste ganho: 80.00
+-- Em percentual: 10 %
+
+-- 2000.00
+
+-- Novo salario: 2140.00
+-- Reajuste ganho: 140.00
+-- Em percentual: 7 %
+
+-- SOLUÇÃO IF
+DO
+$$
+DECLARE
+    salario numeric(10, 2) := valor_aleatorio_entre(0, 5000);
+    reajuste numeric(10, 2);
+    novo_salario numeric(10, 2);
+    percentual int;
+BEGIN
+    IF salario <= 400 THEN
+        percentual := 15;
+    ELSIF salario <= 800 THEN
+        percentual := 12;
+    ELSIF salario <= 1200 THEN
+        percentual := 10;
+    ELSIF salario <= 2000 THEN
+        percentual := 7;
+    ELSE
+        percentual := 4;
+    END IF;
+    reajuste := salario * percentual / 100;
+    RAISE NOTICE 'Novo salario: %', salario + reajuste;
+    RAISE NOTICE 'Reajuste ganho: %', reajuste;
+    RAISE NOTICE 'Em percentual: % %%', percentual;
+END 
+$$;
+
+-- SOLUÇÃO CASE
+DO
+$$
+DECLARE
+    salario numeric(10, 2) := valor_aleatorio_entre(0, 5000);
+    reajuste numeric(10, 2);
+    novo_salario numeric(10, 2);
+    percentual int;
+BEGIN
+    CASE
+        WHEN salario <= 400 THEN
+            percentual := 15;
+        WHEN salario <= 800 THEN
+            percentual := 12;
+        WHEN salario <= 1200 THEN
+            percentual := 10;
+        WHEN salario <= 2000 THEN
+            percentual := 7;
+        ELSE
+            percentual := 4;
+    END CASE;
+    reajuste := salario * percentual / 100;
+    RAISE NOTICE 'Novo salario: %', salario + reajuste;
+    RAISE NOTICE 'Reajuste ganho: %', reajuste;
+    RAISE NOTICE 'Em percentual: % %%', percentual;
+END 
+$$;
